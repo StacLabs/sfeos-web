8ca96366456dcaaa433fe5d1b3207f3766bb9218
jonhealy1 <jonathan.d.healy@gmail.com>
2025-11-05 14:14:32 +0800

    make link on main logo

--- sfeos-web/src/components/LogoOverlay.js
+++ sfeos-web/src/components/LogoOverlay.js
@@ -4,11 +4,13 @@ import './LogoOverlay.css';
 const LogoOverlay = () => {
   return (
     <div className="logo-overlay">
-      <img 
-        src={`${process.env.PUBLIC_URL}/assets/sfeos-logo.png`} 
-        alt="SFEOS Logo"
-        className="logo-image"
-      />
+      <a href="https://github.com/stac-utils/stac-fastapi-elasticsearch-opensearch" target="_blank" rel="noopener noreferrer">
+        <img 
+          src={`${process.env.PUBLIC_URL}/assets/sfeos-logo.png`} 
+          alt="SFEOS Logo"
+          className="logo-image"
+        />
+      </a>
     </div>
   );
 };
